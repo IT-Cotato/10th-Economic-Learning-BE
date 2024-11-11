ca75031280d0a5c7dc4f2a617cf210361392c606
chaen-ing <kimce0405@naver.com>
2024-11-11 17:51:30 +0900

    Chore : 스프링 부트 프로젝트 생성

---
+++ src/test/java/com/ripple/BE/RippleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ripple.BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RippleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
