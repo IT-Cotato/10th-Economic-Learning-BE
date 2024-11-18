4b2125debeead78b4bb312dd2c70d6dd8097c64e
chaen-ing <kimce0405@naver.com>
2024-11-18 17:36:43 +0900

    Feat : 에러 응답 코드 작성 (#1)

---
+++ src/main/java/com/ripple/BE/global/exception/response/ErrorResponse.java
@@ -0,0 +1,39 @@
+package com.ripple.BE.global.exception.response;
+
+import java.util.List;
+
+import org.springframework.validation.FieldError;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Builder;
+
+@Builder
+public record ErrorResponse(
+	Boolean isSuccess,
+	String code,
+	String message,
+	@JsonInclude(JsonInclude.Include.NON_EMPTY)    //유효성 오류가 있을 때만 포함
+	ValidationErrors results
+) {
+	public record ValidationErrors(
+		List<ValidationError> validationErrors
+	) {
+
+	}
+
+	//@Valid 오류의 경우 사용 - error 발생 이유
+	@Builder
+	public record ValidationError(
+		String field,
+		String message
+	) {
+		// Spring의 FieldError 객체를 ValidationError 객체로 변환
+		public static ValidationError from(final FieldError fieldError) {
+			return ValidationError.builder()
+				.field(fieldError.getField())
+				.message(fieldError.getDefaultMessage())
+				.build();
+		}
+	}
+}
