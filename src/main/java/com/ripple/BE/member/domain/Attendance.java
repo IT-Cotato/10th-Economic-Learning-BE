99069d8eddbb81b099b316ef0f5003ebec61d27e
chaen-ing <kimce0405@naver.com>
2024-11-25 22:15:54 +0900

    Feat : Attendance 출석 구현 (#7)

---
+++ src/main/java/com/ripple/BE/member/domain/Attendance.java
@@ -0,0 +1,68 @@
+package com.ripple.BE.member.domain;
+
+import java.time.LocalDateTime;
+
+import com.ripple.BE.global.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "attendance")
+@Getter
+@Builder
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Attendance extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "attendance_id", nullable = false)
+	private Long id;
+
+	@Column(name = "monday")
+	private Boolean monday;
+
+	@Column(name = "tuesday")
+	private Boolean tuesday;
+
+	@Column(name = "wednesday")
+	private Boolean wednesday;
+
+	@Column(name = "thursday")
+	private Boolean thursday;
+
+	@Column(name = "friday")
+	private Boolean friday;
+
+	@Column(name = "saturday")
+	private Boolean saturday;
+
+	@Column(name = "sunday")
+	private Boolean sunday;
+
+	@Column(name = "current_streak")
+	private Long currentStreak;
+
+	@Column(name = "last_reset_date")
+	private LocalDateTime lastResetDate;
+
+	@Column(name = "last_attended_date")
+	private LocalDateTime lastAttendedDate;
+
+	@OneToOne
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+}
