539caaa45c833287c0097d87ebfdc24190e310b1
chaen-ing <kimce0405@naver.com>
2024-12-10 16:01:29 +0900

    Debug : 불필요한 애노테이션 제거 (#12)

--- src/main/java/com/ripple/BE/auth/dto/KakaoUserInfoResponse.java
+++ src/main/java/com/ripple/BE/auth/dto/KakaoUserInfoResponse.java
@@ -1,7 +1,4 @@
 package com.ripple.BE.auth.dto;
 
-import lombok.Getter;
-
-@Getter
 public record KakaoUserInfoResponse(
         Long id, String connected_at, KakaoProperties properties, KakaoAccount kakao_account) {}
