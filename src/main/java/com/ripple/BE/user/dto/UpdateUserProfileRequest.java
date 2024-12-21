a47b0c62125f3ef724f82c44764114bc1743598a
chaen-ing <kimce0405@naver.com>
2024-12-21 23:34:27 +0900

    Feat : 유저 정보 입력 필드들 수정 (#12)

--- src/main/java/com/ripple/BE/user/dto/UpdateUserProfileRequest.java
+++ src/main/java/com/ripple/BE/user/dto/UpdateUserProfileRequest.java
@@ -5,14 +5,12 @@ import com.ripple.BE.user.domain.type.BusinessType;
 import com.ripple.BE.user.domain.type.Gender;
 import com.ripple.BE.user.domain.type.Job;
 import jakarta.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 
 public record UpdateUserProfileRequest(
         @NotNull String nickname,
         @NotNull BusinessType businessType,
         @NotNull Job job,
-        AgeRange ageRange,
-        LocalDateTime birthDay,
+        @NotNull AgeRange ageRange,
         Gender gender,
         String profileIntro,
         Boolean isLearningAlarmAllowed,
