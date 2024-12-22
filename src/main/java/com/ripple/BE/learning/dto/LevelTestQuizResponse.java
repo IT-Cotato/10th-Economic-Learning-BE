5936bcc89f6f6105a866ef3426e5dea06c98324e
chaen-ing <kimce0405@naver.com>
2024-12-22 14:55:00 +0900

    Feat : 레벨테스트 문제 조회 type 추가 (#14)

--- src/main/java/com/ripple/BE/learning/dto/LevelTestQuizResponse.java
+++ src/main/java/com/ripple/BE/learning/dto/LevelTestQuizResponse.java
@@ -1,13 +1,16 @@
 package com.ripple.BE.learning.dto;
 
 import com.ripple.BE.learning.domain.Quiz;
+import com.ripple.BE.learning.domain.type.Type;
 import lombok.Builder;
 
 @Builder
-public record LevelTestQuizResponse(Long id, String question, String answer, String wrongAnswer) {
+public record LevelTestQuizResponse(
+        Long id, Type type, String question, String answer, String wrongAnswer) {
     public static LevelTestQuizResponse from(Quiz quiz) {
         return LevelTestQuizResponse.builder()
                 .id(quiz.getId())
+                .type(quiz.getType())
                 .question(quiz.getQuestion())
                 .answer(quiz.getAnswer())
                 .wrongAnswer(quiz.getWrongAnswer())
