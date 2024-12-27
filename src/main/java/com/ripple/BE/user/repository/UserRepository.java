9437882c17dcc21bb2d1cc63c524ea0e25f56408
jeongchanmin <jjw421412@naver.com>
2024-12-27 16:43:03 +0900

    Feat : DB 조회 함수 추가 (#15) - Fetch join 추가 - 비관적 락 함수 추가

--- src/main/java/com/ripple/BE/user/repository/UserRepository.java
+++ src/main/java/com/ripple/BE/user/repository/UserRepository.java
@@ -1,12 +1,20 @@
 package com.ripple.BE.user.repository;
 
 import com.ripple.BE.user.domain.User;
+import jakarta.persistence.LockModeType;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByKeyCode(String keyCode);
 
     Optional<User> findByAccountEmail(String accountEmail);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select u from User u")
+    List<User> findAllWithLock();
 }
