c429891bc4c0c32ef00dee26e044920d6eea039f
jeongchanmin <jjw421412@naver.com>
2024-12-27 16:46:22 +0900

    Feat : 학습 기능 예외처리 코드 추가 (#15)

--- src/main/java/com/ripple/BE/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/ripple/BE/global/exception/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.ripple.BE.global.exception.errorcode.GlobalErrorCode;
 import com.ripple.BE.global.exception.response.ErrorResponse;
 import com.ripple.BE.global.exception.response.ErrorResponse.ValidationError;
 import com.ripple.BE.global.exception.response.ErrorResponse.ValidationErrors;
+import com.ripple.BE.learning.exception.LearningException;
 import com.ripple.BE.user.exception.UserException;
 import io.micrometer.common.lang.NonNull;
 import java.util.List;
@@ -65,6 +66,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(UserException.class)
     public ResponseEntity<Object> handleUserException(final UserException e) {
+        return handleExceptionInternal(e.getErrorCode());
+    }
+
+    @ExceptionHandler(LearningException.class)
+    public ResponseEntity<Object> handleLearningException(final LearningException e) {
         return handleExceptionInternal(e.getErrorCode());
     }
 
