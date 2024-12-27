e2a96e7ad23636b4ea2e64b5ee524901c1cec7d8
jeongchanmin <jjw421412@naver.com>
2024-12-27 16:48:44 +0900

    Feat : 컨트롤러 계층 개념 DTO 추가 (#15)

---
+++ src/main/java/com/ripple/BE/learning/dto/response/ConceptResponse.java
@@ -0,0 +1,16 @@
+package com.ripple.BE.learning.dto.response;
+
+import com.ripple.BE.learning.dto.ConceptDTO;
+
+public record ConceptResponse(
+        Long conceptId, // 개념 ID
+        String name, // 개념 이름
+        String explanation, // 개념 설명
+        String example // 개념 예시
+        ) {
+
+    public static ConceptResponse toConceptResponse(final ConceptDTO conceptDTO) {
+        return new ConceptResponse(
+                conceptDTO.conceptId(), conceptDTO.name(), conceptDTO.explanation(), conceptDTO.example());
+    }
+}
