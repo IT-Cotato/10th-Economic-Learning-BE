d1a6640acb07f8833c10a48e65779b94b860e762
jeongchanmin <jjw421412@naver.com>
2024-12-27 16:46:17 +0900

    Feat : 학습 기능 예외처리 코드 추가 (#15)

---
+++ src/main/java/com/ripple/BE/learning/exception/errorcode/LearningErrorCode.java
@@ -0,0 +1,17 @@
+package com.ripple.BE.learning.exception.errorcode;
+
+import com.ripple.BE.global.exception.errorcode.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum LearningErrorCode implements ErrorCode {
+    LEARNING_SET_NOT_FOUND(HttpStatus.NOT_FOUND, "Learning set not found"),
+    SAVE_LEARNING_SET_EXCEL_FILE_FAILED(
+            HttpStatus.INTERNAL_SERVER_ERROR, "Failed to save learning set excel file");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
