0e221c8e113b738368aa1ec3f6827f7e9522fe53
jeongchanmin <jjw421412@naver.com>
2025-01-03 15:56:05 +0900

    Feat : 게시물 기능 관련 예외 및 예외 코드 추가 (#21)

---
+++ src/main/java/com/ripple/BE/post/exception/errorcode/PostErrorCode.java
@@ -0,0 +1,21 @@
+package com.ripple.BE.post.exception.errorcode;
+
+import com.ripple.BE.global.exception.errorcode.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum PostErrorCode implements ErrorCode {
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post not found"),
+    TOKTOK_POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Toktok post not found"),
+    LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "Like not found"),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "Comment not found"),
+    TODAY_TOKTOK_NOT_FOUND(HttpStatus.NOT_FOUND, "TodayToktok not found"),
+
+    LIKE_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "Like already exists");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
