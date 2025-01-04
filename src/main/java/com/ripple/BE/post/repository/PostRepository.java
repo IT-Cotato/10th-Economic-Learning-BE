b530f32c53e10470a2416576a6fd89accd5fd325
jeongchanmin <jjw421412@naver.com>
2025-01-04 19:07:47 +0900

    Feat : 카테고리별 게시물 조회 기능 추가 (#21)

--- src/main/java/com/ripple/BE/post/repository/PostRepository.java
+++ src/main/java/com/ripple/BE/post/repository/PostRepository.java
@@ -6,6 +6,8 @@ import io.lettuce.core.dynamic.annotation.Param;
 import jakarta.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -22,4 +24,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query(
             "SELECT p FROM Post p WHERE p.type = :type AND p.id NOT IN (SELECT t.post.id FROM TodayToktok t)")
     List<Post> findUnselectedPosts(@Param("type") PostType type);
+
+    Page<Post> findByType(PostType type, Pageable pageable);
 }
