87a77599a0823f17a7ddae7bfa782e7dbbbb5b18
jeongchanmin <jjw421412@naver.com>
2025-01-08 11:02:28 +0900

    Refactor : 메서드명 변경 (#15)

--- src/main/java/com/ripple/BE/learning/dto/response/RandomQuizResponse.java
+++ src/main/java/com/ripple/BE/learning/dto/response/RandomQuizResponse.java
@@ -4,15 +4,16 @@ import com.ripple.BE.learning.domain.type.Type;
 import com.ripple.BE.learning.dto.QuizDTO;
 import java.util.List;
 
-public record QuizResponse(
+public record RandomQuizResponse(
         Long quizId, // 퀴즈 ID
         Type type, // 퀴즈 타입
         String question, // 문제
         List<String> options // 보기, 선지
         ) {
 
-    public static QuizResponse toQuizResponse(final QuizDTO quizDTO) {
+    public static RandomQuizResponse toQuizResponse(final QuizDTO quizDTO) {
 
-        return new QuizResponse(quizDTO.id(), quizDTO.type(), quizDTO.question(), quizDTO.options());
+        return new RandomQuizResponse(
+                quizDTO.id(), quizDTO.type(), quizDTO.question(), quizDTO.options());
     }
 }
