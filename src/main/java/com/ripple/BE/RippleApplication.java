342c6ce5c1061e3ea9fad5d61e45170678287247
jeongchanmin <jjw421412@naver.com>
2025-01-17 03:29:41 +0900

    bug : nginx 도커 컨테이너 추가 (#23)

--- src/main/java/com/ripple/BE/RippleApplication.java
+++ src/main/java/com/ripple/BE/RippleApplication.java
@@ -2,11 +2,10 @@ package com.ripple.BE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class RippleApplication {
 
     public static void main(String[] args) {
