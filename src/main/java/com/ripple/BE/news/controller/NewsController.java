a32605352b779e20bffdb5446effb58ec0892a63
jeongchanmin <jjw421412@naver.com>
2025-01-24 17:30:25 +0900

    Refactor : api 명 변경  (#28)

--- src/main/java/com/ripple/BE/news/controller/NewsController.java
+++ src/main/java/com/ripple/BE/news/controller/NewsController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/api/news")
 @RequiredArgsConstructor
 public class NewsController {
 
