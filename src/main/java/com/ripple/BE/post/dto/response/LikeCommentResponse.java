7ef0584ba0d335fb0a9d947e8e0330ac56a1ff77
chaen-ing <kimce0405@naver.com>
2025-01-24 23:57:54 +0900

    Refact : response에서 createDate 포맷 설정 (#25)

--- src/main/java/com/ripple/BE/post/dto/response/LikeCommentResponse.java
+++ src/main/java/com/ripple/BE/post/dto/response/LikeCommentResponse.java
@@ -1,17 +1,17 @@
 package com.ripple.BE.post.dto.response;
 
+import com.ripple.BE.global.utils.RelativeTimeFormatter;
 import com.ripple.BE.post.domain.type.PostType;
 import com.ripple.BE.post.dto.LikeCommentDTO;
-import java.time.LocalDateTime;
 
 public record LikeCommentResponse(
-        Long id, String content, String postName, PostType type, LocalDateTime createdDate) {
+        Long id, String content, String postName, PostType type, String createdDate) {
     public static LikeCommentResponse toLikeCommentResponse(LikeCommentDTO likeCommentDTO) {
         return new LikeCommentResponse(
                 likeCommentDTO.id(),
                 likeCommentDTO.content(),
                 likeCommentDTO.postName(),
                 likeCommentDTO.type(),
-                likeCommentDTO.createdDate());
+                RelativeTimeFormatter.formatRelativeTime(likeCommentDTO.createdDate()));
     }
 }
