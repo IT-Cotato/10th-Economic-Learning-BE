a6be5c6af0e78cd07e21eea38c147439dd8dc517
jeongchanmin <jjw421412@naver.com>
2025-01-24 17:04:20 +0900

    Feat : 각 카테고리별 뉴스 크롤러 추가  (#28)

---
+++ src/main/java/com/ripple/BE/news/crawler/NewsCrawler.java
@@ -0,0 +1,68 @@
+package com.ripple.BE.news.crawler;
+
+import static com.ripple.BE.news.exception.errorcode.NewsErrorCode.*;
+
+import com.ripple.BE.news.domain.type.NewsCategory;
+import com.ripple.BE.news.dto.NewsDTO;
+import com.ripple.BE.news.exception.NewsException;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+@Slf4j
+public abstract class NewsCrawler {
+
+    protected static final String LIST_SELECTOR = "li.sa_item"; // 리스트 선택자
+    protected static final String TITLE_SELECTOR = "strong.sa_text_strong"; // 제목 선택자
+    protected static final String CONTENT_SELECTOR = "div.sa_text_lede"; // 내용 선택자
+    protected static final String PUBLISH_SELECTOR = "div.sa_text_press"; // 언론사 선택자
+    protected static final String CONTENT_URL_SELECTOR = "div.sa_text a"; // 본문 URL 선택자
+
+    /** 크롤링 기본 메서드 어제 날짜에 해당하는 데이터만 출력 */
+    public List<NewsDTO> crawl() {
+
+        log.info("Crawling started");
+
+        String baseUrl = getPageUrl(); // 크롤링할 URL
+        List<NewsDTO> newsList = new ArrayList<>();
+
+        try {
+
+            Elements elementList = Jsoup.connect(baseUrl).get().select(LIST_SELECTOR);
+            if (elementList.isEmpty()) {
+                return newsList;
+            }
+
+            for (Element element : elementList) {
+                String title = element.select(TITLE_SELECTOR).text();
+                String content = element.select(CONTENT_SELECTOR).text();
+                String publisher = element.select(PUBLISH_SELECTOR).text();
+                String contentUrl = element.select(CONTENT_URL_SELECTOR).attr("href");
+
+                if (!title.isEmpty() && !content.isEmpty()) {
+                    NewsDTO newsDTO =
+                            NewsDTO.toNewsDTO(title, content, publisher, contentUrl, getNewsCategory());
+                    newsList.add(newsDTO);
+                }
+            }
+
+        } catch (Exception e) {
+            throw new NewsException(NEWS_INTERNAL_SERVER_ERROR);
+        }
+
+        return newsList;
+    }
+
+    /**
+     * 크롤링할 페이지 URL 반환
+     *
+     * @return 페이지 URL
+     */
+    protected abstract String getPageUrl();
+
+    /** 카테고리 반환 */
+    protected abstract NewsCategory getNewsCategory();
+}
---
+++ src/main/java/com/ripple/BE/news/crawler/impl/InvestmentNewsCrawler.java
@@ -0,0 +1,28 @@
+package com.ripple.BE.news.crawler.impl;
+
+import com.ripple.BE.news.crawler.NewsCrawler;
+import com.ripple.BE.news.domain.type.NewsCategory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class InvestmentNewsCrawler extends NewsCrawler {
+
+    protected static final String URL = "https://news.naver.com/breakingnews/section/101/258";
+
+    /**
+     * 페이지 URL
+     *
+     * @return 투자 페이지 URL
+     */
+    @Override
+    public String getPageUrl() {
+        return URL;
+    }
+
+    @Override
+    public NewsCategory getNewsCategory() {
+        return NewsCategory.INVESTMENT;
+    }
+}
