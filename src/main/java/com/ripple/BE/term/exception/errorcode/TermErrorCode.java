2f117ffbf8542261ad5cda247aae6e210574a651
jeongchanmin <jjw421412@naver.com>
2025-01-24 23:07:53 +0900

    Feat : 스크랩 관련 에러 코드 추가 (#33)

--- src/main/java/com/ripple/BE/term/exception/errorcode/TermErrorCode.java
+++ src/main/java/com/ripple/BE/term/exception/errorcode/TermErrorCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum TermErrorCode implements ErrorCode {
     TERM_NOT_FOUND(HttpStatus.NOT_FOUND, "Term not found"),
-    SAVE_TERM_EXCEL_FILE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to save term excel file");
+    TERM_SCRAP_NOT_FOUND(HttpStatus.NOT_FOUND, "Term scrap not found"),
+
+    SAVE_TERM_EXCEL_FILE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to save term excel file"),
+    TERM_SCRAP_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "Term scrap already exist");
 
     private final HttpStatus httpStatus;
     private final String message;
