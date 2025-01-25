a37c434143c97728c7f5face5d5379cb5b65a51c
chaen-ing <kimce0405@naver.com>
2025-01-25 17:28:30 +0900

    Feat : 도메인 필드 수정 (#24)

--- src/main/java/com/ripple/BE/chatbot/domain/ChatMessage.java
+++ src/main/java/com/ripple/BE/chatbot/domain/ChatMessage.java
@@ -6,12 +6,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
@@ -40,8 +37,4 @@ public class ChatMessage extends BaseEntity {
     @Enumerated(EnumType.STRING)
     @Column(name = "sender", nullable = false)
     private Sender sender; // 메시지 송신자
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "chat_session_id")
-    private ChatSession chatSession;
 }
