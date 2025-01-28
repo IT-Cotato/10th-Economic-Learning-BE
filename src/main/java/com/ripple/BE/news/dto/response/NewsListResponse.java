64d6c8ae647c1da9d39c5edf1348a8fb93f7aef8
jeongchanmin <jjw421412@naver.com>
2025-01-28 13:25:13 +0900

    Refactor : 목록 조회 시 미리보기 DTO 추가 및 수정 (#35)

--- src/main/java/com/ripple/BE/news/dto/response/NewsListResponse.java
+++ src/main/java/com/ripple/BE/news/dto/response/NewsListResponse.java
@@ -3,11 +3,11 @@ package com.ripple.BE.news.dto.response;
 import com.ripple.BE.news.dto.NewsListDTO;
 import java.util.List;
 
-public record NewsListResponse(List<NewsResponse> newsList, int totalPage, int currentPage) {
+public record NewsListResponse(List<NewsPreviewResponse> newsList, int totalPage, int currentPage) {
 
     public static NewsListResponse toNewsListResponse(NewsListDTO newsListDTO) {
         return new NewsListResponse(
-                newsListDTO.newsDTOList().stream().map(NewsResponse::toNewsResponse).toList(),
+                newsListDTO.newsDTOList().stream().map(NewsPreviewResponse::toNewsPreviewResponse).toList(),
                 newsListDTO.totalPage(),
                 newsListDTO.currentPage());
     }
