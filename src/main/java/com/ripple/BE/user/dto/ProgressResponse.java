c6ff9fef4d7b5f1a5c63cbe4b895e4ab195dfeff
jeongchanmin <jjw421412@naver.com>
2025-02-02 15:21:46 +0900

    Refactor : 레벨별 진도율 로직 수정 - 캐싱  추가 (#49)

--- src/main/java/com/ripple/BE/user/dto/ProgressResponse.java
+++ src/main/java/com/ripple/BE/user/dto/ProgressResponse.java
@@ -3,11 +3,18 @@ package com.ripple.BE.user.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.ripple.BE.user.domain.type.Level;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record ProgressResponse(Map<Level, Double> progress) {
+public record ProgressResponse(Map<Level, Integer> progress) {
 
     public static ProgressResponse toProgressResponse(final ProgressDTO levelDTO) {
-        return new ProgressResponse(levelDTO.progress());
+
+        Map<Level, Integer> progressMap =
+                levelDTO.progress().entrySet().stream()
+                        .collect(
+                                Collectors.toMap(Map.Entry::getKey, entry -> (int) Math.round(entry.getValue())));
+
+        return new ProgressResponse(progressMap);
     }
 }
