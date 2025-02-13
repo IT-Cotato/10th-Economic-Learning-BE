12591278572f4242bcd0954a9df9c027506eddca
chaen-ing <kimce0405@naver.com>
2025-02-13 18:04:44 +0900

    Debug : 경제톡톡 author 문제 (#68)

--- src/main/java/com/ripple/BE/user/dto/CommunityUserDTO.java
+++ src/main/java/com/ripple/BE/user/dto/CommunityUserDTO.java
@@ -5,6 +5,10 @@ import com.ripple.BE.user.domain.User;
 public record CommunityUserDTO(Long id, String nickname, String profileImageUrl) {
 
     public static CommunityUserDTO toCommunityUserDTO(final User user) {
+        if (user == null) {
+            return new CommunityUserDTO(null, null, null);
+        }
+
         return new CommunityUserDTO(
                 user.getId(),
                 user.getNickname(),
