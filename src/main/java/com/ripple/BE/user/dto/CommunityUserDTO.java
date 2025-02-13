0449ba7e6bba03c34acdb273fdc83fe6736c8630
chaen-ing <kimce0405@naver.com>
2025-02-13 17:59:34 +0900

    Debug : 프로필 이미지 반환 로직 변경 (#68)

--- src/main/java/com/ripple/BE/user/dto/CommunityUserDTO.java
+++ src/main/java/com/ripple/BE/user/dto/CommunityUserDTO.java
@@ -1,11 +1,13 @@
 package com.ripple.BE.user.dto;
 
-import com.ripple.BE.image.domain.Image;
 import com.ripple.BE.user.domain.User;
 
-public record CommunityUserDTO(Long id, String nickname, Image profileImage) {
+public record CommunityUserDTO(Long id, String nickname, String profileImageUrl) {
 
     public static CommunityUserDTO toCommunityUserDTO(final User user) {
-        return new CommunityUserDTO(user.getId(), user.getNickname(), user.getProfileImage());
+        return new CommunityUserDTO(
+                user.getId(),
+                user.getNickname(),
+                user.getProfileImage() == null ? null : user.getProfileImage().getS3Info().getUrl());
     }
 }
