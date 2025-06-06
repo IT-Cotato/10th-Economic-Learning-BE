3007d5d6eeb478494c58c16a3f067b7ef4442241
jeongchanmin <jjw421412@naver.com>
2025-06-06 21:41:51 +0900

    refactor : deleteAllByPostId 쿼리 수정

--- src/main/java/com/ripple/BE/post/persistence/jpa/repository/commentlike/CommentLikeJpaRepository.java
+++ src/main/java/com/ripple/BE/post/persistence/jpa/repository/commentlike/CommentLikeJpaRepository.java
@@ -1,22 +1,34 @@
 package com.ripple.BE.post.persistence.jpa.repository.commentlike;
 
 import com.ripple.BE.post.persistence.jpa.entity.CommentLikeJpaEntity;
+
 import io.lettuce.core.dynamic.annotation.Param;
+
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CommentLikeJpaRepository
-        extends JpaRepository<CommentLikeJpaEntity, Long>, CommentLikeQueryRepository {
+	extends JpaRepository<CommentLikeJpaEntity, Long>, CommentLikeQueryRepository {
 
-    Optional<CommentLikeJpaEntity> findByCommentIdAndUserId(long commentId, long userId);
+	Optional<CommentLikeJpaEntity> findByCommentIdAndUserId(long commentId, long userId);
 
-    boolean existsByCommentIdAndUserId(long commentId, long userId);
+	boolean existsByCommentIdAndUserId(long commentId, long userId);
 
-    void deleteAllByCommentId(long commentId);
+	void deleteAllByCommentId(long commentId);
 
-    @Modifying
-    @Query("DELETE FROM CommentLikeJpaEntity cl WHERE cl.comment.post.id = :postId")
-    void deleteAllByPostId(@Param("postId") Long postId);
+	@Modifying
+	@Query("""
+		    DELETE FROM CommentLikeJpaEntity cl
+		    WHERE cl.commentId IN (
+		        SELECT c.id FROM CommentJpaEntity c WHERE c.postId = :postId
+		    )
+		""")
+	void deleteAllByPostId(@Param("postId") Long postId);
+
+	List<CommentLikeJpaEntity> findByUserIdAndCommentIdIn(long userId, Set<Long> commentIds);
 }
