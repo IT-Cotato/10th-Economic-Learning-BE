7feff9058750347fd384bebd00c2e8826f2c9b82
jeongchanmin <jjw421412@naver.com>
2025-06-07 22:24:28 +0900

    refactor : 불필요 파일 삭제

--- src/main/java/com/ripple/BE/news/persistence/newscrap/NewsScrapRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.ripple.BE.news.persistence.newscrap;
-
-import com.ripple.BE.news.domain.News;
-import java.util.List;
-
-public interface NewsScrapRepositoryCustom {
-
-    List<News> findNewsScrappedByUser(Long userId);
-}
