d7ee53e5866e437ab192c36df901949c07c8ab3e
jeongchanmin <jjw421412@naver.com>
2025-06-10 21:55:55 +0900

    refactor : 불필요 파일 삭제

--- src/main/java/com/ripple/BE/term/repository/TermRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ripple.BE.term.repository;
-
-import com.ripple.BE.term.domain.Term;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TermRepository extends JpaRepository<Term, Long>, TermRepositoryCustom {}
