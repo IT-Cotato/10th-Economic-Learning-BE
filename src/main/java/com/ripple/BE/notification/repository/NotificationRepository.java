e214315e9de42c9efadb25cdefbc717249992572
jeongchanmin <jjw421412@naver.com>
2025-06-13 21:31:40 +0900

    refactor : 불필요 파일 삭제 (#101)

--- src/main/java/com/ripple/BE/notification/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ripple.BE.notification.repository;
-
-import com.ripple.BE.notification.domain.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository
-        extends JpaRepository<Notification, Long>, NotificationRepositoryCustom {}
