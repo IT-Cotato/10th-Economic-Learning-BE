3e31f610fa43cdcfadd165e25e01e143c8b705e6
jeongchanmin <jjw421412@naver.com>
2025-06-16 22:15:01 +0900

    refactor: 이미지 리포지토리 의존성 분리 및 계층 구조 정리 (#105)
    - ImageRepositoryImpl: JPA 의존성을 캡슐화하여 DIP 원칙 적용
    - ImageService 위치를 application 계층으로 이동하여 계층 명확화
    - 패키지 구조를 application/persistence로 구분하여 역할별 책임 분리

--- src/main/java/com/ripple/BE/image/application/ImageService.java
+++ src/main/java/com/ripple/BE/image/application/ImageService.java
@@ -1,11 +1,11 @@
-package com.ripple.BE.image.service;
+package com.ripple.BE.image.application;
 
 import static com.ripple.BE.image.exception.errorcode.ImageErrorCode.*;
 
 import com.ripple.BE.image.domain.Image;
 import com.ripple.BE.image.domain.S3Info;
 import com.ripple.BE.image.exception.ImageException;
-import com.ripple.BE.image.repository.ImageRepository;
+import com.ripple.BE.image.persistence.ImageRepository;
 import com.ripple.BE.image.s3.S3Uploader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 @Service
 @Slf4j
+@Transactional
 public class ImageService {
 
     private final ImageRepository imageRepository;
@@ -24,25 +25,22 @@ public class ImageService {
     private static final String FOLDER_NAME = "post";
     private static final String TOKTOK_FOLDER_NAME = "toktok";
 
-    @Transactional
     public long addImageToPost(MultipartFile file) {
 
         S3Info s3Info = s3Uploader.uploadFiles(file, FOLDER_NAME);
 
-        Image image = imageRepository.save(Image.toImageEntity(s3Info));
+        Image image = imageRepository.save(Image.withoutId(s3Info, null, null));
         return image.getId();
     }
 
-    @Transactional
     public String addImageToToktok(MultipartFile file) {
 
         S3Info s3Info = s3Uploader.uploadFiles(file, TOKTOK_FOLDER_NAME);
 
-        Image image = imageRepository.save(Image.toImageEntity(s3Info));
+        Image image = imageRepository.save(Image.withoutId(s3Info, null, null));
         return image.getS3Info().getUrl();
     }
 
-    @Transactional
     public void deleteImage(long imageId) {
         Image image =
                 imageRepository.findById(imageId).orElseThrow(() -> new ImageException(IMAGE_NOT_FOUND));
@@ -51,12 +49,11 @@ public class ImageService {
         imageRepository.delete(image);
     }
 
-    @Transactional
     public long addProfileImage(MultipartFile file) {
 
         S3Info s3Info = s3Uploader.uploadFiles(file, "profile");
 
-        Image image = imageRepository.save(Image.toImageEntity(s3Info));
+        Image image = imageRepository.save(Image.withoutId(s3Info, null, null));
         return image.getId();
     }
 }
