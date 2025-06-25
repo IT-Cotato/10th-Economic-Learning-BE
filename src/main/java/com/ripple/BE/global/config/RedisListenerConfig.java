b490f0dd99d665026a1ad82cec473a5ee70606fc
jeongchanmin <jjw421412@naver.com>
2025-06-25 16:22:39 +0900

    chore: Redis Pub/Sub 캐시 무효화 설정 추가 (#115)
    - RedisConfig 및 RedisListenerConfig 추가
    - RedisListener에서 토픽 구독 및 무효화 처리

--- src/main/java/com/ripple/BE/global/config/RedisListenerConfig.java
+++ src/main/java/com/ripple/BE/global/config/RedisListenerConfig.java
@@ -1,6 +1,7 @@
 package com.ripple.BE.global.config;
 
 import com.ripple.BE.notification.application.redis.RedisNotificationSubscriber;
+import com.ripple.BE.post.application.cache.redis.PostCacheEvictionSubscriber;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,7 @@ import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 public class RedisListenerConfig {
 
     private final RedisNotificationSubscriber notificationSubscriber;
+    private final PostCacheEvictionSubscriber postCacheEvictionSubscriber;
 
     /** 알림용 Redis 채널 토픽 빈 설정 */
     @Bean
@@ -21,10 +23,21 @@ public class RedisListenerConfig {
         return new ChannelTopic("notification");
     }
 
+    /** 캐시 무효화를 위한 Redis 채널 토픽 빈 설정 */
+    @Bean
+    public ChannelTopic postCacheEvictTopic() {
+        return new ChannelTopic("post-cache-evict");
+    }
+
     /** 알림 메시지를 처리하는 subscriber 설정 추가 */
     @Bean
     public MessageListenerAdapter listenerAdapterNotification() {
         return new MessageListenerAdapter(notificationSubscriber, "onNotification");
+    }
+
+    @Bean
+    public MessageListenerAdapter listenerAdapterCacheEvict() {
+        return new MessageListenerAdapter(postCacheEvictionSubscriber, "onEvictMessage");
     }
 
     /** redis 에 발행(publish)된 메시지 처리를 위한 리스너 컨테이너 설정 */
@@ -39,6 +52,9 @@ public class RedisListenerConfig {
 
         // notificationTopic에 대한 메시지 리스너 어댑터 설정
         container.addMessageListener(listenerAdapterNotification, notificationTopic);
+
+        // postCacheEvictTopic에 대한 메시지 리스너 어댑터 설정
+        container.addMessageListener(listenerAdapterCacheEvict(), postCacheEvictTopic());
 
         return container;
     }
