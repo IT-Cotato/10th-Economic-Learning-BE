441ff4c33e9a5f178c135e9601d12d59c0613504
jeongchanmin <jjw421412@naver.com>
2025-07-01 01:02:48 +0900

    fix: 전역 예외 처리 방식 수정 (#117)

--- src/main/java/com/ripple/BE/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/ripple/BE/global/exception/handler/GlobalExceptionHandler.java
@@ -71,10 +71,10 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
         // SSE 요청일 경우 빈 응답 또는 연결 닫기 처리
         if (MediaType.TEXT_EVENT_STREAM_VALUE.equals(contentType)) {
-            e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
 
+        logger.error("예외 발생: {}", e);
         return handleExceptionInternal(GlobalErrorCode.INTERNAL_SERVER_ERROR);
     }
 
