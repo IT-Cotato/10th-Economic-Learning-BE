77a9d125a6e57deb8da5be4efc84944fda46f71e
chaen-ing <kimce0405@naver.com>
2025-07-20 23:47:38 +0900

    Feat : count sql natural 모드로 변경 (#141)

--- src/main/java/com/ripple/BE/term/persistence/jdbc/TermJdbcRepository.java
+++ src/main/java/com/ripple/BE/term/persistence/jdbc/TermJdbcRepository.java
@@ -64,7 +64,7 @@ public class TermJdbcRepository {
 		String countSql = """
 			SELECT COUNT(*)
 			FROM terms t
-			WHERE MATCH(t.title, t.description) AGAINST (? IN BOOLEAN MODE)
+			WHERE MATCH(t.title, t.description) AGAINST (? IN NATURAL LANGUAGE MODE)
 			""";
 
 		return getTermWithScrapDTOS(keyword, pageable, userId, searchSql, countSql);
